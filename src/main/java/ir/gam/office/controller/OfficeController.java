11ea898044036470e722412b77d43359a387411d
farahani <mfarahani31@yahoo.com>
2019-12-25 11:47:49 +0330

    OfficeController reformatted Flyway added to project

--- src/main/java/ir/gam/office/controller/OfficeController.java
+++ src/main/java/ir/gam/office/controller/OfficeController.java
@@ -37,29 +37,11 @@ public class OfficeController {
         return ResponseEntity.status(HttpStatus.CREATED).body(office);
     }
 
-//    @RequestMapping(value = "/getOffices", method = RequestMethod.GET)
-//    public @ResponseBody
-//    List<OfficeDto> getAllOffices() {
-//        List<Office> offices = officeService.findAllOffices();
-//        if (offices.isEmpty())
-//            throw new OfficeNotfoundException("ids-" + offices);
-//        return OfficeMapper.INSTANCE.officesToOfficesDto(offices);
-//    }
-
 
     @RequestMapping(value = "/getOffices", method = RequestMethod.GET)
     public ResponseEntity<List<OfficeDto>> findAllOffices() {
         return ResponseEntity.ok(officeMapper.INSTANCE.officesToOfficesDto(officeService.findAllOffices()));
     }
-
-    //    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-//    public OfficeDto editOffice(OfficeDto officeDto,@PathVariable("id") Long id) {
-//        Office office = officeService.findById(id);
-//        office.setName(officeDto.getName());
-//        office.setProviderName(officeDto.getProviderName());
-//        return ;
-//    }
-
 
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
     public ResponseEntity<OfficeDto> update(@PathVariable Long id, @RequestBody OfficeDto officeDto) {
@@ -71,24 +53,12 @@ public class OfficeController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(officeDto);
     }
 
-//    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-//    public @ResponseBody
-//    List<OfficeDto> deleteOffice(@PathVariable("id") Long id) {
-//        officeService.deleteById(id);
-//        List<Office> offices = officeService.findAllOffices();
-//        if (offices.isEmpty())
-//            throw new OfficeNotfoundException("ids-" + offices);
-//        return officeMapper.INSTANCE.officesToOfficesDto(offices);
-//    }
-
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public ResponseEntity delete(@PathVariable Long id) {
         officeService.deleteById(id);
 
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
-
-
 
 
 }
